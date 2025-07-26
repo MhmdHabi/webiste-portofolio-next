ff0f74f3df2934dee646ae43a9ec4c7ae1c8e376
Muhammad Habi <muhammadhabi01@gmail.com>
2025-07-26 21:40:13 +0700

    perbaikan frontend

--- global.d.ts
+++ global.d.ts
@@ -1,7 +1,14 @@
 export {};
 
-declare module "*.glb";
-declare module "*.png";
+declare module "*.glb" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.png" {
+  const src: string;
+  export default src;
+}
 
 declare module "meshline" {
   export const MeshLineGeometry: any;
@@ -16,8 +23,3 @@ declare global {
     }
   }
 }
-
--src / vite - env.d.ts;
-/// <reference types="vite/client" />
-declare module "*.glb";
-declare module "*.png";
