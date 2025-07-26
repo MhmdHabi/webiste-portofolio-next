27e84f9b2ea8bb81e25063e55488ff2006fb0e75
Muhammad Habi <muhammadhabi01@gmail.com>
2025-07-26 20:57:09 +0700

    add use client

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { FaReact, FaPython, FaNodeJs, FaVuejs, FaAngular, FaStripe, FaAws, FaJava } from "react-icons/fa";
